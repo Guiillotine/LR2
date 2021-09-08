1ff4540d4ce8b124b21b7412c89455deb7645b8d
Guiillotine <90143027+Guiillotine@users.noreply.github.com>
2021-09-08 22:56:57 +0700

    Добавлена функция "добавление нового коровника"

--- LR2.cpp
+++ LR2.cpp
@@ -7,18 +7,19 @@
 using namespace std;
 void add_cow(struct cow cow1[], struct food food1[], int& num_cow, int num_food);
 void add_food(struct food food1[], int& num_food);
+void add_cowshed(struct cowshed cowshed1[], int& num_cowsheed);
 
 int main()
 {
 	struct cow cow1[200] = {};
 	struct food food1[200] = {};
-	int num_cow, num_food;
-	num_cow = 0;
-	num_food = 0;
+	struct cowshed cowshed1[200] = {};
+	int num_cow = 0, num_food = 0, num_cowsheed = 0;
 	//cow1 = (cow*)malloc(20 * sizeof(cow));
 	//food1 = (food*)malloc(20 * sizeof(food));
 	num_food++;
 	strcpy(food1[0].name, "Овёс");
+	add_cowshed(cowshed1, num_cowsheed);
 	add_food(food1, num_food);
 	add_cow(cow1, food1, num_cow, num_food);
 	_getch();
--- LR2.h
+++ LR2.h
@@ -15,16 +15,15 @@ struct cow
 	char name[50];
 	char breed[100]; //порода
 	char food[50];
-	char sex;
 	int age;
 	double weight;
+	int nadoi; //Надой
 };
 
 struct food
 {
 	int id;
 	char name[50];
-	//string company; //фирма
 	char company[70]; //фирма
 	double price;
 };
@@ -34,8 +33,8 @@ struct cowshed //Коровник
 	int id;
 	int S; //Площадь
 	int kol_cows; // Сколько помещается коров
-	int price; //Стоимость коровника
-	int exp; //Ежемесячные затраты на обслуживание
+	double price; //Стоимость коровника
+	double exp; //Ежемесячные затраты на обслуживание
 };
 
 struct warehouse //склад
@@ -62,44 +61,36 @@ void add_cow(struct cow cow1[], struct food food1[], int& num_cow, int num_food)
 {
 	SetConsoleCP(1251); //задаем кодировку для вывода символов на экран
 	SetConsoleOutputCP(1251); //задаем кодировку для ввода символов с клавиатуры в консоль
-	int a;
+	int a; 
 	//system("cls");
 	printf("\n Добавление новой коровы");
 	cow1[num_cow].id = num_cow + 1;
 
 	printf("\n Введите кличку коровы: ");
-	//gets_s(cow1[num_cow].name);
 	std::cin >> cow1[num_cow].name; while (getchar() != '\n');
 
 	printf("\n Введите породу коровы: ");
 	gets_s(cow1[num_cow].breed); //gets_s тобы вводить с пробелами
-	//std::cin >> cow1[num_cow].breed;
 
 
 	printf("\n Выберите корм для коровы: ");
 	for (int i = 0; i < num_food; i++) printf("\n %d->%s", i+1, food1[i].name);
 	printf("\n ");
-	scanf("%d", &a);
+	std::cin >> a;
 	while (getchar() != '\n');
 	for (int i = 0; i < num_food; i++)
 		if (a == (i + 1)) strcpy(cow1[num_cow].food, food1[i].name);
 
-	printf("\n Выберите пол коровы (М/Ж): ");
-	/*scanf("%c", &cow1[num_cow].sex);
-	while (getchar() != '\n');*/
-	std::cin >> cow1[num_cow].sex;
-
 	printf("\n Введите возраст коровы (лет): ");
-	/*scanf("%d", &cow1[num_cow].age);
-	while (getchar() != '\n');*/
 	std::cin >> cow1[num_cow].age;
 
 	printf("\n Введите вес коровы (кг): ");
-	/*scanf("%d", &cow1[num_cow].weight);
-	while (getchar() != '\n');*/
 	std::cin >> cow1[num_cow].weight;
 
-	printf("\n № %d\n КЛИЧКА %s\n ПОРОДА %s\n КОРМ %s\n ПОЛ %c\n ВОЗРАСТ %d\n ВЕС %.3f", cow1[num_cow].id, cow1[num_cow].name, cow1[num_cow].breed, cow1[num_cow].food, cow1[num_cow].sex, cow1[num_cow].age, cow1[num_cow].weight);
+	printf("\n Введите надой коровы (л/сут): ");
+	std::cin >> cow1[num_cow].nadoi;
+
+	printf("\n № %d\n КЛИЧКА %s\n ПОРОДА %s\n КОРМ %s\n ВОЗРАСТ(лет) %d\n ВЕС(кг) %.3f\n НАДОЙ(л/сут) %d", cow1[num_cow].id, cow1[num_cow].name, cow1[num_cow].breed, cow1[num_cow].food, cow1[num_cow].age, cow1[num_cow].weight, cow1[num_cow].nadoi);
 	num_cow++;
 }
 
@@ -109,7 +100,7 @@ void add_food(struct food food1[], int& num_food)
 	SetConsoleCP(1251); //задаем кодировку для вывода символов на экран
 	SetConsoleOutputCP(1251); //задаем кодировку для ввода символов с клавиатуры в консоль
 	int a;
-	system("cls");
+	//system("cls");
 	printf("\n Добавление нового корма");
 	food1[num_food].id = num_food + 1;
 
@@ -119,9 +110,36 @@ void add_food(struct food food1[], int& num_food)
 	printf("\n Введите фирму-производителя корма: ");
 	std::cin >> food1[num_food].company;
 
-	printf("\n Введите цену корма: ");
+	printf("\n Введите цену корма (в рублях): ");
 	std::cin >> food1[num_food].price;
 
-	printf("\n № %d\n НАЗВАНИЕ %s\n ФИРМА %s\n ЦЕНА %.3f\n\n", food1[num_food].id, food1[num_food].name, food1[num_food].company, food1[num_food].price);
+	printf("\n № %d\n НАЗВАНИЕ %s\n ФИРМА %s\n ЦЕНА(в рублях) %.3f\n\n", food1[num_food].id, food1[num_food].name, food1[num_food].company, food1[num_food].price);
 	num_food++;
 }
+
+
+void add_cowshed (struct cowshed cowshed1[], int& num_cowsheed)
+{
+	SetConsoleCP(1251); //задаем кодировку для вывода символов на экран
+	SetConsoleOutputCP(1251); //задаем кодировку для ввода символов с клавиатуры в консоль
+	int a;
+	system("cls");
+	printf("\n Добавление нового коровника");
+	cowshed1[num_cowsheed].id = num_cowsheed + 1;
+
+	printf("\n Укажите площадь коровника(м^2): ");
+	std::cin >> cowshed1[num_cowsheed].S;
+
+	printf("\n Укажите вместимость коровника (количество животных): ");
+	std::cin >> cowshed1[num_cowsheed].kol_cows;
+
+	printf("\n Укажите стоимость коровника (в рублях): ");
+	std::cin >> cowshed1[num_cowsheed].price;
+
+	printf("\n Укажите ежемесячные затраты на обслуживание (в рублях): ");
+	std::cin >> cowshed1[num_cowsheed].exp;
+
+	printf("\n № %d\n ПЛОЩАДЬ КОРОВНИКА(м^2) %d\n ВМЕСТИМОСТЬ КОРОВНИКА(количество животных) %d\n СТОИМОСТЬ КОРОВНИКА(в рублях) %.3f\n ЕЖЕМЕСЯЧНЫЕ ЗАТРАТЫ НА ОБСЛУЖИВАНИЕ(в рублях) %.3f\n\n", cowshed1[num_cowsheed].id, cowshed1[num_cowsheed].S, cowshed1[num_cowsheed].kol_cows, cowshed1[num_cowsheed].price, cowshed1[num_cowsheed].exp);
+	num_cowsheed++;
+}
+
